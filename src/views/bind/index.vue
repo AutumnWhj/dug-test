9aea0a2a790765844fbfd11d717ab5db07736ddd
Jin <439661734@qq.com>
2023-02-15 16:21:03 +0800

    fix: fix bug

--- src/views/bind/index.vue
+++ src/views/bind/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center relative mt-12">
-    <div class="w-1/2 card-shadow flex flex-col bg-white py-10 px-8 rounded-3xl">
+    <div class="w-2/5 card-shadow flex flex-col bg-white py-10 px-8 rounded-3xl">
       <div class="w-full flex flex-col">
         <div class="text-center mb-8">
           <div class="text-gray-300 text-xl mb-3">{{ restaurantId ? '首次绑定外卖平台' : '完善绑定外卖平台' }} </div>
@@ -234,6 +234,7 @@
           if (typeof message === 'string') {
             ElMessage.warning(message);
           }
+          toAddRestaurant();
         }
       } else {
         console.log('error submit!', fields);
