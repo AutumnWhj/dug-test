45a97a7ebe782bb02b6682a31c066b2a004c1c67
Jin <439661734@qq.com>
2023-03-15 19:11:11 +0800

    fix: register local

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -3,24 +3,24 @@
     <el-image class="h-28 w-28 mb-10" :src="logoTextImage" />
     <div class="min-w-96 card-shadow flex flex-col bg-white py-10 px-8 rounded-3xl">
       <div class="text-center mb-8">
-        <div class="text-gray-300 text-xl mb-3">注册</div>
-        <div class="text-gray-300/50 text-sm">创建帐户以继续！</div>
+        <div class="text-gray-300 text-xl mb-3">{{ $t('register.register') }}</div>
+        <div class="text-gray-300/50 text-sm">{{ $t('register.continue') }}</div>
       </div>
       <el-form class="w-full" ref="ruleFormRef" :model="ruleForm" :rules="rules" size="large">
         <el-form-item prop="email">
-          <el-input v-model="ruleForm.email" placeholder="你的邮箱" />
+          <el-input v-model="ruleForm.email" :placeholder="$t('register.form.email')" />
         </el-form-item>
         <el-form-item prop="username">
-          <el-input v-model="ruleForm.username" placeholder="你的名字" />
+          <el-input v-model="ruleForm.username" :placeholder="$t('register.form.username')" />
         </el-form-item>
         <el-form-item prop="phone_number">
-          <el-input v-model="ruleForm.phone_number" placeholder="你的电话" />
+          <el-input v-model="ruleForm.phone_number" :placeholder="$t('register.form.phone_number')" />
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="ruleForm.password" placeholder="密码" type="password" />
+          <el-input v-model="ruleForm.password" :placeholder="$t('register.form.password')" type="password" />
         </el-form-item>
         <el-form-item prop="repeatPassword">
-          <el-input v-model="ruleForm.repeatPassword" placeholder="再次输入密码" type="password" />
+          <el-input v-model="ruleForm.repeatPassword" :placeholder="$t('register.form.repeatPassword')" type="password" />
         </el-form-item>
         <el-form-item>
           <password-strength :password="ruleForm.password" style="padding-top: 10px" />
@@ -28,16 +28,16 @@
       </el-form>
       <div class="w-full flex justify-between items-center mb-8">
         <el-checkbox v-model="isAgreement" label="记住密码" size="small">
-          通过创建帐户，您同意我们的
-          <span class="text-primary text-xs cursor-pointer hover:text-blue-500/70">条款和条件</span>
+          {{ $t('register.checkbox[0]') }}
+          <span class="text-primary text-xs cursor-pointer hover:text-blue-500/70"> {{ $t('register.checkbox[1]') }}</span>
         </el-checkbox>
       </div>
       <el-button class="w-full !rounded-3xl mb-8" :disabled="!isAgreement" size="large" color="#2C72FE" @click="submitForm(ruleFormRef)">
-        Register
+        {{ $t('register.btn') }}
       </el-button>
       <div class="text-center">
-        <span class="text-gray-300/50 text-xs"> 已经有账户了？</span>
-        <span class="text-primary text-xs cursor-pointer hover:text-blue-500/70" @click="toLogin">登录</span>
+        <span class="text-gray-300/50 text-xs"> {{ $t('register.hadAccount') }}</span>
+        <span class="text-primary text-xs cursor-pointer hover:text-blue-500/70" @click="toLogin">{{ $t('register.login') }}</span>
       </div>
     </div>
   </div>
@@ -46,11 +46,13 @@
 <script lang="ts" setup>
   import { useRouter } from 'vue-router';
   import { ElMessage } from 'element-plus';
-  import type { FormInstance, FormRules } from 'element-plus';
+  import type { FormInstance } from 'element-plus';
   import passwordStrength from '/@/components/PasswordStrength/index.vue';
   import logoTextImage from '/@/assets/images/logo_text.png';
   import { register } from '/@/api/user';
   import { useUserStore } from '/@/store';
+  import { useI18n } from 'vue-i18n';
+  const { t } = useI18n();
   const userStore = useUserStore();
 
   const ruleFormRef = ref<FormInstance>();
@@ -63,38 +65,40 @@
   });
   const validatorPassword = (_rule, value, callback) => {
     if (value && (value.length < 8 || value.length > 16)) {
-      callback('密码长度限制8~16位');
+      callback(t('register.rules.validatorPassword'));
     } else {
       callback();
     }
   };
-  const rules = reactive<FormRules>({
-    email: [
-      { required: true, message: '请输入邮箱', trigger: 'blur' },
-      {
-        pattern:
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        message: '请输入正确的邮箱',
-        trigger: 'blur',
-      },
-    ],
-    username: [
-      { required: true, message: '请输入用户名', trigger: 'blur' },
-      {
-        pattern: /^[\u4E00-\u9FA5A-Za-z0-9]+$/,
-        message: '用户名不能带有特殊符号',
-        trigger: 'blur',
-      },
-    ],
-    phone_number: [{ required: true, message: '请输入电话', trigger: 'blur' }],
-    password: [
-      { required: true, message: '请输入密码', trigger: 'blur' },
-      { validator: validatorPassword, trigger: 'blur' },
-    ],
-    repeatPassword: [
-      { required: true, message: '请再次输入密码', trigger: 'blur' },
-      { validator: validatorPassword, trigger: 'blur' },
-    ],
+  const rules = computed(() => {
+    return {
+      email: [
+        { required: true, message: t('register.rules.email'), trigger: 'blur' },
+        {
+          pattern:
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          message: t('register.rules.pattern'),
+          trigger: 'blur',
+        },
+      ],
+      username: [
+        { required: true, message: t('register.rules.username'), trigger: 'blur' },
+        {
+          pattern: /^[\u4E00-\u9FA5A-Za-z0-9]+$/,
+          message: t('register.rules.usernamePattern'),
+          trigger: 'blur',
+        },
+      ],
+      phone_number: [{ required: true, message: t('register.rules.phone_number'), trigger: 'blur' }],
+      password: [
+        { required: true, message: t('register.rules.password'), trigger: 'blur' },
+        { validator: validatorPassword, trigger: 'blur' },
+      ],
+      repeatPassword: [
+        { required: true, message: t('register.rules.repeatPassword'), trigger: 'blur' },
+        { validator: validatorPassword, trigger: 'blur' },
+      ],
+    };
   });
 
   const isAgreement = ref(false);
@@ -104,11 +108,11 @@
       if (valid) {
         console.log('submit!', ruleForm);
         if (ruleForm.repeatPassword !== ruleForm.password) {
-          ElMessage.warning('两次输入的密码不一致');
+          ElMessage.warning(t('register.message.warning'));
           return;
         }
         await register(ruleForm);
-        ElMessage.success('注册成功');
+        ElMessage.success(t('register.message.success'));
         await userStore.login({
           username: ruleForm.username,
           password: ruleForm.password,
